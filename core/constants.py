f3363be2e126daa78d93efbe248e53f0ef257984
guspleesamutr-stack <guspleesamutr@gmail.com>
2026-07-06 15:12:59 +0700

    Update constants.py

--- core/constants.py
+++ core/constants.py
@@ -15,3 +15,37 @@ HOVER_DELAY = 200
 HOVER_SIZE = (650, 450)
 
 THUMBNAIL_SIZE = (160, 90)
+
+# =========================================================
+# DATA
+# =========================================================
+
+BACKHAULS = [
+    "LAN1G+LAN1G",
+    "5G+5G",
+    "LAN1G+5G",
+    "5G+LAN1G",
+    "MLO+MLO",
+    "LAN2.5G+LAN1G",
+    "LAN1G+LAN2.5G",
+    "MLO+LAN1G",
+    "LAN1G+MLO",
+    "LAN2.5G+MLO",
+    "MLO+LAN2.5G",
+    "5G+LAN2.5G",
+    "LAN2.5G+5G"
+]
+
+LAPTOPS = [
+    "Laptop wifi6",
+    "Laptop wifi7",
+    "Other"
+]
+
+MOBILES = [
+    "P30",
+    "P40",
+    "S10",
+    "IQOO13",
+    "Other"
+]
