d038299e90a5f12cf33518cb5671a17667825709
guspleesamutr-stack <guspleesamutr@gmail.com>
2026-07-06 14:50:36 +0700

    Create filename engine

---
+++ core/filename_engine.py
@@ -0,0 +1,14 @@
+"""
+Filename generation engine.
+
+This module is responsible for generating
+folder names and filenames for both
+Standalone and Mesh test modes.
+"""
+
+
+class FilenameEngine:
+    """Generate folder and filename."""
+
+    def __init__(self):
+        pass
