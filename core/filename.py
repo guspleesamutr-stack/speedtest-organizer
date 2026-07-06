044f3a9045e1de5e005b57b338b71e6b80960f5a
guspleesamutr-stack <guspleesamutr@gmail.com>
2026-07-06 14:53:11 +0700

    Create filename module

---
+++ core/filename.py
@@ -0,0 +1,16 @@
+"""
+Filename utility.
+
+This module is responsible for generating
+folder names and filenames.
+"""
+
+
+def build_filename():
+    """
+    Placeholder function.
+
+    The actual implementation will be moved
+    from main.py in the next step.
+    """
+    pass
