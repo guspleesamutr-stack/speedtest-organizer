56ada092a718df547e0c4f3abfe58966b6cff2c5
guspleesamutr-stack <guspleesamutr@gmail.com>
2026-07-06 15:33:32 +0700

    Add mesh WiFi filename builder

--- core/filename.py
+++ core/filename.py
@@ -20,3 +20,27 @@ def build_standalone_filename(
         filename += f"_CH{channel}"
 
     return filename
+
+def build_mesh_wifi_filename(
+    topology,
+    backhaul,
+    router,
+    distance,
+    band,
+    device,
+    channel,
+):
+    filename = (
+        f"Mesh_"
+        f"{topology}_"
+        f"{backhaul}_"
+        f"{router}_"
+        f"{distance}_"
+        f"{band}_"
+        f"{device}"
+    )
+
+    if channel:
+        filename += f"_CH{channel}"
+
+    return filename
